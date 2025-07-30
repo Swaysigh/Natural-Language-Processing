de299c1cf839e7549879448dc2a0a9a305a022e5
Swayam <assentioncreed3@gmail.com>
2025-07-30 15:16:09 +0530

    Create EXP-2.py

---
+++ EXP-2.py
@@ -0,0 +1,8 @@
+#Q2. Convert text into tokens and print the tokens in the sentence.
+
+sentence_1 = "Hello, my name is Swayam Singh. I am a student at the CSVTU and I love programming and exploring new technologies."
+sentence_2 = "myself Swayam Singh, a student at CSVTU, loves programming and exploring new technologies."
+tokens = sentence_1.split()
+list = list(sentence_2)
+print("Tokens in the sentence:", tokens)
+print("Another token:", list)
