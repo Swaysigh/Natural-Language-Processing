5cbdd958e8363ff0457da0c1957f8504ab3aa28c
Swayam <assentioncreed3@gmail.com>
2025-07-30 15:18:23 +0530

    Update EXP-2.py

--- EXP-2.py
+++ EXP-2.py
@@ -2,7 +2,9 @@
 
 sentence_1 = "Hello, my name is Swayam Singh. I am a student at the CSVTU and I love programming and exploring new technologies."
 sentence_2 = "myself Swayam Singh, a student at CSVTU, loves programming and exploring new technologies."
+
 tokens = sentence_1.split()
-list = list(sentence_2)
+list = [char for char in sentence_2]
+
 print("Tokens in the sentence:", tokens)
 print("Another token:", list)
